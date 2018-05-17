508654e30187cf8dc4ee4e9c3dff1d30db57945f
nikit205 <38934929+nikit205@users.noreply.github.com>
2018-05-17 10:17:07 +0300

    Add files via upload

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour {
 
+
+    public float speed;
 	private Rigidbody rb;
+    
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 	}
-			
-	// Update is called once per frame
-	void Update () {
-		float V = Input.GetAxis ("Vertical");
-		float h = Input.GetAxis ("Horizontal");
 
-		Vector3 dir = new 
-			
-		
-	}
+    // Update is called once per frame
+    void Update() { 
+
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        rb.AddForce(dir * speed);
+    }
+
+
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
---
+++ Assets/rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 r = new Vector3(15, 30, 45);
+        transform.Rotate(r * Time.deltaTime);
+		
+	}
+}
